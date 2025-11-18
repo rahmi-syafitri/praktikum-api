33cba30f350f917873a6482cb953f9a39f6d86df
rahmi-syafitri <rahmisyafitri82@gmail.com>
2025-11-18 21:11:42 +0700

    update

--- app/Models/mahasiswa.php
+++ app/Models/mahasiswa.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Builder\Function_;
 
 class Mahasiswa extends Model
 {
     use HasFactory;
+    protected $table = 'mahasiswa';
+    protected $fillable = ['nama_mahasiswa', 'nim', 'prodi_id'];
 
-    protected $fillable = ['nama_mahasiswa','nim', 'prodi_id'];
+
+    public Function prodi(){
+        return $this->belongsTo(Prodi::class, 'prodi_id');
+    }
 }
